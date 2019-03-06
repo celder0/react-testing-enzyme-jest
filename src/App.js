7256fff3a261531608b4423c7d5f05690b4bfb88
Chris Elder <christopher_elder@homedepot.com>
2019-03-06 13:39:00 -0500

    Finish up this testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
 import React, {Component} from 'react'
 import './App.css'
 
+const teams = [
+    {
+        id: 1,
+        name: "Birmingham Bolts",
+        location: "Birmingham, Alabama",
+        colors: "Purple, Yellow, Silver"
+    },{
+        id: 2,
+        name: "Orlando Rage",
+        location: "Orlando, Florida",
+        colors: "Red, Navy, Gold, White"
+    },{
+        id: 3,
+        name: "Seattle Street Sharks",
+        location: "Seattle, Washington",
+        colors: "Red, Gray, Blue, Black"
+    }];
+
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            foundTeams: []
+        }
+    }
+
+    undraftedPlayerList = (players) => {
+        return players.map((player, index) =>
+            <li data-undrafted-player key={index}>{player}</li>
+        );
+    };
+
+    searchForTeam = (event) => {
+        this.setState( ({
+            foundTeams: teams.filter(team => event.target.value !== '' ?
+                team.name.toLowerCase().includes(event.target.value.toLowerCase()) : false)
+        }));
+    };
+
     render() {
+
+        let {foundTeams} = this.state;
+
         return (
             <div className="App">
+                <h1>Welcome to the Fantasy Football App!</h1>
+                <h3>The central source for all drafting, roster moves, and the latest player information.</h3>
+                <ul>
+                    {this.undraftedPlayerList([
+                        "Bugs Bunny",
+                        "Micky Mouse",
+                        "Roger Rabbit"
+                    ])}
+                </ul>
 
-
+                <input
+                    type="text"
+                    data-team-search-input
+                    onChange={this.searchForTeam}
+                />
+                <ul data-team-search-results>
+                    {foundTeams.map((team, index) => <li key={index}>{team.name} - {team.location} - Colors: {team.colors}</li>)}
+                </ul>
             </div>
         )
     }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,98 @@
-import React from 'react'
-import App from './App'
+import React from 'react';
+import App from './App';
+import {shallow} from 'enzyme';
 
-describe('sanity testing the test suite', () => {
-    it('should work', () => {
-        expect(1).toEqual(2)
+describe("Landing Page", () => {
+    let component;
+
+    beforeEach(()=>{
+        component = shallow(<App/>);
+    });
+
+    it("greets the user", () => {
+
+        let welcomeElement = component.find('h1');
+
+        expect(welcomeElement.exists()).toEqual(true);
+        expect(welcomeElement.text()).toEqual('Welcome to the Fantasy Football App!');
+    });
+
+    it('lets the user know why the site exists', () => {
+        let welcomeElement = component.find('h3');
+
+        expect(welcomeElement.exists()).toEqual(true)
+        expect(welcomeElement.text()).toEqual('The central source for all drafting, roster moves, and the latest player information.')
+    });
+
+    describe('undrafted players', () => {
+        let undraftedPlayers;
+
+        beforeEach(() => {
+            undraftedPlayers = component.find('[data-undrafted-player]');
+        });
+
+        it('shows bugs bunny as a undrafted player', () => {
+            confirmPlayerExistsInList(undraftedPlayers,  "Bugs Bunny" )
+        });
+
+        it('shows micky mouse as a undrafted player', () => {
+            confirmPlayerExistsInList(undraftedPlayers,  "Micky Mouse" )
+        });
+
+        it('shows Roger Rabbit as a undrafted player', () => {
+            confirmPlayerExistsInList(undraftedPlayers,  "Roger Rabbit" )
+        });
+    });
+
+    describe("Searching for teams", () => {
+        describe("when the team is found", () => {
+            it("displays additional information about the teams", () => {
+                let searchResults = searchForTeam("Birmingham Bolts");
+
+                expect(searchResults).toContain("Birmingham Bolts - Birmingham, Alabama - Colors: Purple, Yellow, Silver")
+            });
+
+            it("displays additional information about the teams with partial match on name", () => {
+                let searchResults = searchForTeam("birmingham");
+
+                expect(searchResults).toContain("Birmingham Bolts - Birmingham, Alabama - Colors: Purple, Yellow, Silver")
+            });
+
+            it("displays multiple teams when search matches more than one team", () => {
+                let searchResults = searchForTeam("s");
+                expect(searchResults).toContain("Birmingham Bolts - Birmingham, Alabama - Colors: Purple, Yellow, Silver");
+                expect(searchResults).toContain("Seattle Street Sharks - Seattle, Washington - Colors: Red, Gray, Blue, Black");
+            });
+
+            it("displays no search results if search is empty", ()=>{
+               let searchResults = searchForTeam("");
+               expect(searchResults).toBe("");
+            });
+        })
     })
-})
+});
 
+function confirmPlayerExistsInList(playerList, player){
+    let foundPlayer = playerList.filterWhere(n => {
+        return n.text().includes(player);
+    });
+    expect(foundPlayer).toHaveLength(1);
+}
+
+function searchForTeam(searchTerm){
+    let component = shallow(<App/>);
+
+    let event = {
+        target: {
+            value: searchTerm
+        }
+    };
+
+    component
+        .find("[data-team-search-input]")
+        .simulate("change", event);
+
+    return component
+        .find('[data-team-search-results]')
+        .text();
+}
